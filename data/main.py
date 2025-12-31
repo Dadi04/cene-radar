3e4f86efa9be5b6b378e62c8ac2cb2c527d1840a
Dadi04 <dadica.petkovic@gmail.com>
2025-12-31 19:13:55 +0100

    refactor(data): improve readability and structure
    Split main.py codebase into smaller functions.
    Removed hard-coded numbers.
    No behavior changes.

--- data/main.py
+++ data/main.py
@@ -13,111 +13,136 @@ if not BASE_PATH:
 
 STATE_FILE = Path("download_state.json")
 
-url_1 = "https://data.gov.rs/api/1/datasets/?q=cenovnici"
-url_2 = "https://data.gov.rs/api/2/datasets/?q=cenovnici"
+URLS = [
+    "https://data.gov.rs/api/1/datasets/?q=cenovnici",
+    "https://data.gov.rs/api/2/datasets/?q=cenovnici"
+]
 
-def load_state():
+TOTAL_ORG_NUMBER = 27
+
+def load_state() -> dict:
     if STATE_FILE.exists():
         return json.loads(STATE_FILE.read_text())
     return {}
 
-def save_state(state):
+def save_state(state: dict) -> None:
     STATE_FILE.write_text(json.dumps(state, indent=2))
 
-unique_datasets = {}
-count = 0
+def get_json(url: str, timeout: int = 30) -> dict:
+    response = requests.get(url, timeout=timeout)
+    response.raise_for_status()
+    return response.json()
 
-while (count < 27):
-    response_1 = requests.get(url_1)
-    response_2 = requests.get(url_2)
+def download_file(url: str, target: Path, timeout: int = 60) -> None:
+    response = requests.get(url, stream=True, timeout=timeout)
+    response.raise_for_status()
 
-    response_1.raise_for_status()
-    response_2.raise_for_status()
+    with target.open("wb") as f:
+        for chunk in response.iter_content(chunk_size=8192):
+            if chunk:
+                f.write(chunk)
 
-    data_1 = response_1.json()["data"]
-    data_2 = response_2.json()["data"]
+def fetch_unique_datasets() -> list[dict]:
+    unique = {}
 
-    for item in data_1 + data_2:
-        unique_datasets[item["id"]] = item
+    while len(unique) < TOTAL_ORG_NUMBER:
+        for url in URLS:
+            data = get_json(url).get("data", [])
+            for item in data:
+                unique[item.get("id")] = item
 
-    deduped_data = list(unique_datasets.values())
-    count = len(deduped_data)
+    return list(unique.values())
 
-state = load_state()
+def ensure_resources(dataset: dict) -> list[dict] | None:
+    resources = dataset.get("resources")
 
-for dataset in deduped_data:
-    org = dataset["organization"]
-    org_name = org["name"].replace('"', '').strip()
+    if isinstance(resources, list):
+        return resources
+    
+    # weird data.gov.rs API behaviour
+    # sometimes the resources variable has one resource (its not a list of dicts, but a dict instead)
+    # so you have to get URI of the in that dict first 
+    # and then get the resources from that URI
+    uri = dataset.get("uri")
+    if not uri:
+        print(f"Missing URI for dataset id: {dataset.get("id")}")
+        return None
+    
+    full_dataset = get_json(uri)
+    resources = full_dataset.get("resources")
+    if not isinstance(resources, list):
+        print("Resources still invalid after URI fetch")
+        return None
 
-    last_modified = dataset["last_modified"]
+    return resources
+
+def get_week_folder(last_modified: str) -> str:
     dt = datetime.fromisoformat(last_modified.replace("Z", "+00:00"))
     year, week, _ = dt.isocalendar()
-    week_folder_name = f"{year}-W{week:02d}"
+    return f"{year}-W{week:02d}"
 
-    shop_folder = os.path.join(BASE_PATH, org_name)
-    os.makedirs(shop_folder, exist_ok=True)
+def prepare_folders(org_name: str, week_folder: str) -> Path:
+    org_path = os.path.join(BASE_PATH, org_name)
+    week_path = Path(os.path.join(org_path, week_folder))
 
-    week_folder = os.path.join(shop_folder, week_folder_name)
-    os.makedirs(week_folder, exist_ok=True)
+    week_path.mkdir(parents=True, exist_ok=True)
+    return week_path
 
-    resources = dataset["resources"]
-    if not isinstance(resources, list):
-        uri = dataset.get("uri")
-        if not uri:
-            print("resources is not a list")
+def should_skip(resource: dict, state: dict, week_folder: str) -> bool:
+    resource_id = resource.get("id")
+    last_modified = resource.get("last_modified")
+
+    prev = state.get(resource_id)
+    if not prev:
+        return False
+    
+    if prev["iso_week"] == week_folder and prev["last_modified"] == last_modified:
+        return True
+    return False
+
+def process_resource(resource: dict, target_dir: Path, state: dict, week_folder: str) -> None:
+    if resource.get("filetype") != "file":
+        return None
+    
+    filename = resource.get("title")
+    file_path = Path(os.path.join(target_dir, filename))
+
+    if file_path.exists():
+        print(f"up to date: {filename}")
+        return
+
+    if should_skip(resource, state, week_folder):
+        print(f"skip: {filename}")
+        return
+    
+    print(f"downloading: {filename}")
+    download_file(resource.get("url"), file_path)
+
+    state[resource.get("id")] = {
+        "last_modified": resource.get("last_modified"),
+        "iso_week": week_folder
+    }
+
+    save_state(state)
+    print(f"saved to {file_path}")
+
+def main():
+    state = load_state()
+    datasets = fetch_unique_datasets()
+
+    for dataset in datasets:
+        org = dict(dataset.get("organization"))
+        org_name = org.get("name").replace('"', "").strip()
+
+        week_folder = get_week_folder(dataset.get("last_modified"))
+        target_dir = prepare_folders(org_name, week_folder)
+
+        resources = ensure_resources(dataset)
+        if not resources:
             continue
 
-        response = requests.get(uri, timeout=30)
-        response.raise_for_status()
-        full_dataset = response.json()
+        for resource in resources:
+            if isinstance(resource, dict):
+                process_resource(resource, target_dir, state, week_folder)
 
-        resources = full_dataset.get("resources")
-        if not isinstance(resources, list):
-            print("resources still not a list after uri fetch")
-            continue
-
-    for resource in resources:
-        if not isinstance(resource, dict):
-            print("resource is not a dict")
-            continue
-
-        if resource["filetype"] != "file":
-            print("not a file resource")
-            continue
-
-        resource_id = resource["id"]
-        last_modified = resource["last_modified"]
-        filename = resource["title"]
-
-        prev = state.get(resource_id)
-        if prev:
-            if prev["iso_week"] == week_folder_name and prev["last_modified"] == last_modified:
-                print(f"skip: {filename}")
-                continue
-
-        url = resource["url"]
-        ext = resource["format"]
-        
-        file_path = os.path.join(week_folder, filename)
-        if os.path.exists(file_path):
-            print(f"exists: {filename}")
-            continue
-
-        print(f"downloading: {filename}")
-
-        response = requests.get(url, stream=True, timeout=60)
-        response.raise_for_status()
-
-        with open(file_path, "wb") as f:
-            for chunk in response.iter_content(chunk_size=8192):
-                if chunk:
-                    f.write(chunk)
-
-        state[resource_id] = {
-            "last_modified": last_modified,
-            "iso_week": week_folder_name
-        }
-
-        save_state(state)
-
-        print(f"saved to: {file_path}")
+main()
